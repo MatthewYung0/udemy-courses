09b55647fc9a4a8674a8bbbbcdad28e364558168
MYUNG <myung@volvocars.com>
2022-05-23 14:05:48 +0200

    added Bank, Branch, and Customer classe

--- java-programming-masterclass/section-8-arrays-lists-autoboxing-unboxing/EX44-Banking/Bank.java
+++ java-programming-masterclass/section-8-arrays-lists-autoboxing-unboxing/EX44-Banking/Bank.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Bank {
+
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String branch) {
+
+    }
+
+    public boolean addCustomer(String branch, String customer, double transaction) {
+
+    }
+
+    public boolean addCustomerTransaction(String branch, String customer, double transaction) {
+
+    }
+
+
+    public boolean listCustomers(String branch, boolean transaction) {
+
+    }
+
+    private Branch findBranch(String branch) {
+
+    }
+
 }
--- java-programming-masterclass/section-8-arrays-lists-autoboxing-unboxing/EX44-Banking/Branch.java
+++ java-programming-masterclass/section-8-arrays-lists-autoboxing-unboxing/EX44-Banking/Branch.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Branch {
+
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<Customer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean newCustomer(String name, double transaction) {
+
+    }
+
+    private Customer findCustomer(String customer) {
+
+    }
 }
