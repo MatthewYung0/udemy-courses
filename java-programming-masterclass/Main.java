80fa1d566193e19762c9da44c925f1398d1afebe
MYUNG <myung@volvocars.com>
2022-05-23 09:33:52 +0200

    completed task

--- java-programming-masterclass/Main.java
+++ java-programming-masterclass/Main.java
@@ -2,6 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        MobilePhone newPhone = new MobilePhone("111");
+
+        Contact newContact1 = Contact.createContact("Matthew", "0708921425");
+        newPhone.addNewContact(newContact1);
+        newPhone.printContacts();
+        Contact newContact2 = Contact.createContact("Matthew", "0708921425");
+        newPhone.addNewContact(newContact2);
+        newPhone.printContacts();
+
     }
 
 }
